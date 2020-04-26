574a5f074f7db867018b4551f3ac5ec78cc5365f
gargakshay <gargakshay2010@gmail.com>
2020-04-26 18:42:55 +0530

    Updates

---
+++ precache-manifest.9ef08126b156b8e52b8b30d359ec428d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1863f83a58ecd264fce2c92de3c61c4",
+    "url": "/carousel-react/index.html"
+  },
+  {
+    "revision": "c595875c41ca4c787d6d",
+    "url": "/carousel-react/static/css/main.f0d060fa.chunk.css"
+  },
+  {
+    "revision": "28ed5ac9fdf21ff9886d",
+    "url": "/carousel-react/static/js/2.4cd4c677.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/carousel-react/static/js/2.4cd4c677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c595875c41ca4c787d6d",
+    "url": "/carousel-react/static/js/main.cca19813.chunk.js"
+  },
+  {
+    "revision": "62e04dba5dc6bebfaf76",
+    "url": "/carousel-react/static/js/runtime-main.2422d684.js"
+  },
+  {
+    "revision": "b984f651c66046db6bb695f84edf0c3a",
+    "url": "/carousel-react/static/media/photo-1544194215-541c2d3561a4.b984f651.jpeg"
+  },
+  {
+    "revision": "ada7edb06038a1479fb65dc191679498",
+    "url": "/carousel-react/static/media/photo-1544210163-257effe9399e.ada7edb0.jpeg"
+  },
+  {
+    "revision": "f2be133f853b7349c9c0ec7de3b4574c",
+    "url": "/carousel-react/static/media/photo-1546430783-fe4b9c159e52.f2be133f.jpeg"
+  },
+  {
+    "revision": "4ef64f75d2e331cc59e850a35f1f8aa1",
+    "url": "/carousel-react/static/media/photo-1548438294-1ad5d5f4f063.4ef64f75.jpeg"
+  },
+  {
+    "revision": "d9ee454a07e158a3b5719b3161bb2e53",
+    "url": "/carousel-react/static/media/photo-1553374402-559e8b431161.d9ee454a.jpeg"
+  },
+  {
+    "revision": "fbae8e69afa00221f385876f278acd6f",
+    "url": "/carousel-react/static/media/photo-1554290712-e640351074bd.fbae8e69.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/carousel-react/precache-manifest.24a7279c18fbf8f6df9d20eefcc84019.js"
+  "/carousel-react/precache-manifest.9ef08126b156b8e52b8b30d359ec428d.js"
 );
 
 self.addEventListener('message', (event) => {
