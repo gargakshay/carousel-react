43d6c00205b3bfb8f775a152c1f0542b47bf193e
gargakshay <gargakshay2010@gmail.com>
2020-04-26 16:39:52 +0530

    Updates

---
+++ precache-manifest.c9e64392b0d855f8938a2f82933d8e6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a25bb1fbaf3b7b38c4680a5f1f2333",
+    "url": "/gargakshay/carousel-react/index.html"
+  },
+  {
+    "revision": "a500597b109734390154",
+    "url": "/gargakshay/carousel-react/static/css/main.f0d060fa.chunk.css"
+  },
+  {
+    "revision": "c3c595a4c4de3bceb3a6",
+    "url": "/gargakshay/carousel-react/static/js/2.81e7e537.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gargakshay/carousel-react/static/js/2.81e7e537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a500597b109734390154",
+    "url": "/gargakshay/carousel-react/static/js/main.19309e72.chunk.js"
+  },
+  {
+    "revision": "a699b3e95304c2ed9e7c",
+    "url": "/gargakshay/carousel-react/static/js/runtime-main.6f9e1fc5.js"
+  }
+]);
