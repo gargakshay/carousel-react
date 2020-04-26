1b9c6db414a2795d3d8815d1f5738940b77c4774
gargakshay <gargakshay2010@gmail.com>
2020-04-26 19:03:11 +0530

    Cosmetic changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,56 +7,60 @@ function App() {
     <img
       src={require("./assets/images/photo-1544194215-541c2d3561a4.jpeg")}
       alt=""
-    ></img>,
+    />,
     <img
       src={require("./assets/images/photo-1544210163-257effe9399e.jpeg")}
       alt=""
-    ></img>,
+    />,
     <img
       src={require("./assets/images/photo-1546430783-fe4b9c159e52.jpeg")}
       alt=""
-    ></img>,
+    />,
     <img
       src={require("./assets/images/photo-1548438294-1ad5d5f4f063.jpeg")}
       alt=""
-    ></img>,
+    />,
     <img
       src={require("./assets/images/photo-1553374402-559e8b431161.jpeg")}
       alt=""
-    ></img>,
+    />,
     <img
       src={require("./assets/images/photo-1554290712-e640351074bd.jpeg")}
       alt=""
-    ></img>,
+    />,
     <img
       src={require("./assets/images/photo-1544194215-541c2d3561a4.jpeg")}
       alt=""
-    ></img>,
+    />,
     <img
       src={require("./assets/images/photo-1544210163-257effe9399e.jpeg")}
       alt=""
-    ></img>,
+    />,
     <img
       src={require("./assets/images/photo-1546430783-fe4b9c159e52.jpeg")}
       alt=""
-    ></img>,
+    />,
     <img
       src={require("./assets/images/photo-1548438294-1ad5d5f4f063.jpeg")}
       alt=""
-    ></img>,
+    />,
     <img
       src={require("./assets/images/photo-1553374402-559e8b431161.jpeg")}
       alt=""
-    ></img>,
+    />,
     <img
       src={require("./assets/images/photo-1554290712-e640351074bd.jpeg")}
       alt=""
-    ></img>
+    />
   ];
 
   const list2 = [];
   for (let i = 0; i < 30; i++) {
-    list2.push(<div className="block"><span className="block-area">{i}</span></div>);
+    list2.push(
+      <div className="block">
+        <span className="block-area">{i}</span>
+      </div>
+    );
   }
 
   return (
